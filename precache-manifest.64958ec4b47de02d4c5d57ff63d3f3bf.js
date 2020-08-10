f2c54ef3260e98fedea97315e7e556e071bcead5
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-08-10 21:59:40 +0530

    Updates

---
+++ precache-manifest.64958ec4b47de02d4c5d57ff63d3f3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1990671c257e1af2352dfbd70a1dcf76",
+    "url": "/Landing-page/index.html"
+  },
+  {
+    "revision": "c4cb7e39bc1b2f5a112e",
+    "url": "/Landing-page/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2133b50afc315f492eeb",
+    "url": "/Landing-page/static/css/main.7f9ae025.chunk.css"
+  },
+  {
+    "revision": "c4cb7e39bc1b2f5a112e",
+    "url": "/Landing-page/static/js/2.5671e941.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Landing-page/static/js/2.5671e941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2133b50afc315f492eeb",
+    "url": "/Landing-page/static/js/main.9349f155.chunk.js"
+  },
+  {
+    "revision": "ce2d3a89e33a3ecfe777",
+    "url": "/Landing-page/static/js/runtime-main.e62ce239.js"
+  }
+]);
